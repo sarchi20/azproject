4d687f88c337a2a2c726b1c697b615cba609178e
sarchi20 <117329456+sarchi20@users.noreply.github.com>
2023-03-18 21:07:41 -0400

    Update vm1.sh
    updated sequence

--- vm1.sh
+++ vm1.sh
@@ -4,8 +4,8 @@ sudo apt-get update
 sudo apt-get install python3
 sudo apt-get install python3-pip
 sudo pip3 install azure.storage
+sudo pip3 install azure
 sudo pip3 install configparser
 sudo pip3 install flask
-sudo pip install --upgrade pyinstaller
-sudo pip3 install azure
 sudo pip3 install azure-storage-blob==2.1.0
+sudo pip3 install --upgrade pyinstaller

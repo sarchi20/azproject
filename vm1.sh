7b383b0a333fb3dfaf5fbcbe02d5f6034c64d8c8
sarchi20 <117329456+sarchi20@users.noreply.github.com>
2023-03-18 21:02:52 -0400

    Update vm1.sh
    updated with new installation

--- vm1.sh
+++ vm1.sh
@@ -3,6 +3,9 @@ sudo ufw allow 5000/tcp
 sudo apt-get update
 sudo apt-get install python3
 sudo apt-get install python3-pip
-pip3 install azure.storage
-pip3 install configparser
-pip3 install flask
+sudo pip3 install azure.storage
+sudo pip3 install configparser
+sudo pip3 install flask
+sudo pip install --upgrade pyinstaller
+sudo pip3 install azure
+sudo pip3 install azure-storage-blob==2.1.0
